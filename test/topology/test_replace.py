f7c774125303a72930f5cc6e8381d887a9861ee4
Patryk Jędrzejczak <patryk.jedrzejczak@scylladb.com>
2023-11-06 16:25:09 +0100

    test: add test_replacing_alive_node_fails
    We add a test for the Raft-based topology's new feature - failing
    the replace operation if the node being replaced is consider alive
    by the failure detector.

--- test/topology/test_replace.py
+++ test/topology/test_replace.py
@@ -6,6 +6,7 @@
 """
 Test replacing node in different scenarios
 """
+import asyncio
 import time
 from test.pylib.scylla_cluster import ReplaceConfig
 from test.pylib.manager_client import ManagerClient
@@ -48,3 +49,17 @@ async def test_replace_reuse_ip_using_host_id(manager: ManagerClient) -> None:
     replace_cfg = ReplaceConfig(replaced_id = servers[0].server_id, reuse_ip_addr = True, use_host_id = True)
     await manager.server_add(replace_cfg)
     await wait_for_token_ring_and_group0_consistency(manager, time.time() + 30)
+
+@pytest.mark.asyncio
+async def test_replacing_alive_node_fails(manager: ManagerClient) -> None:
+    """Try replacing an alive node and check that it fails"""
+    servers = await manager.running_servers()
+    await asyncio.gather(*(manager.server_sees_others(srv.server_id, len(servers) - 1) for srv in servers))
+
+    # We test for every server because we expect a different error depending on
+    # whether we try to replace the topology coordinator. We want to test both cases.
+    # Both errors contain the expected string below.
+    for srv in servers:
+        replace_cfg = ReplaceConfig(replaced_id = srv.server_id, reuse_ip_addr = False, use_host_id = False)
+        await manager.server_add(replace_cfg=replace_cfg,
+                                 expected_error="the topology coordinator rejected request to join the cluster")
