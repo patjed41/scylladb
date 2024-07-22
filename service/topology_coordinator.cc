f72583e843662b7a13dc773c1e10874d9ad4920a
Patryk Jędrzejczak <patryk.jedrzejczak@scylladb.com>
2024-07-22 11:47:27 +0200

    token_metadata: rename get_all_endpoints and get_all_ips
    In one of the following patches, we introduce support for zero-token
    nodes. A zero-token node that has successfully joined the cluster is
    in the normal state but is not a normal token owner. Hence, the names
    of `get_all_endpoints` and `get_all_ips` become unclear. They should
    specify that the functions return only token owners.

--- service/topology_coordinator.cc
+++ service/topology_coordinator.cc
@@ -2612,7 +2612,7 @@ future<> topology_coordinator::build_coordinator_state(group0_guard guard) {
     auto sl_version = co_await _sys_ks.get_service_levels_version();
     if (!sl_version || *sl_version < 2) {
         rtlogger.info("migrating service levels data");
-        co_await qos::service_level_controller::migrate_to_v2(tmptr->get_all_endpoints().size(), _sys_ks, _sys_ks.query_processor(), _group0.client(), _as);
+        co_await qos::service_level_controller::migrate_to_v2(tmptr->get_normal_token_owners().size(), _sys_ks, _sys_ks.query_processor(), _group0.client(), _as);
     }
 
     rtlogger.info("building initial raft topology state and CDC generation");
@@ -2651,7 +2651,7 @@ future<> topology_coordinator::build_coordinator_state(group0_guard guard) {
     std::set<sstring> enabled_features;
 
     // Build per-node state
-    for (const auto& host_id: tmptr->get_all_endpoints()) {
+    for (const auto& host_id: tmptr->get_normal_token_owners()) {
         const auto ep = tmptr->get_endpoint_for_host_id_if_known(host_id);
         if (!ep) {
             throw std::runtime_error(format("failed to build initial raft topology state from gossip for node with ID {}, as its IP is not known", host_id));
