25122a389da2f11272a99cb735057046af1e80f2
Emil Maskovsky <emil.maskovsky@scylladb.com>
2025-05-20 11:43:55 +0200

    take the logger as a parameter
    Take the logger from the outside to use a single logger in the script

--- scripts/hackaton/raft_recovery/remove_dead_nodes.py
+++ scripts/hackaton/raft_recovery/remove_dead_nodes.py
@@ -8,16 +8,15 @@ from typing import List
 from .ssh_helper import create_ssh_client
 from .nodetool_helper import NodetoolHelper
 
-logger = logging.getLogger(__name__)
 
-
-def remove_dead_nodes(ssh: paramiko.SSHClient, nodetool: NodetoolHelper, host_ids: List[str]) -> None:
+def remove_dead_nodes(ssh: paramiko.SSHClient, nodetool: NodetoolHelper, host_ids: List[str], logger: logging.Logger) -> None:
     """
     Removes each host ID in the list using the provided NodetoolHelper and SSH client session.
     Args:
         ssh: An established paramiko.SSHClient session to the initiator node.
         nodetool: NodetoolHelper instance to use for nodetool operations.
         host_ids: List of host IDs to remove from the cluster.
+        logger: Logger instance to use for logging.
     """
     logger.debug(f"Removing nodes: {host_ids} using provided SSH session and nodetool at {nodetool.nodetool_path}")
     for host_id in host_ids:
