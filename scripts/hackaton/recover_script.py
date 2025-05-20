d50253a7cb22b90efb5ead57ec3884227862b6b3
Emil Maskovsky <emil.maskovsky@scylladb.com>
2025-05-20 11:55:02 +0200

    initialize the SSH client
    Add the parameters to provide the SSH user and key for logging in to the
    remote server.

--- scripts/hackaton/recover_script.py
+++ scripts/hackaton/recover_script.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 import argparse
-
+from raft_recovery.ssh_helper import create_ssh_client
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Start a recover procedure")
-    parser.add_argument("--node", action="store", required=True,help="Provide a node to connect to")
+    parser.add_argument("--node", action="store", required=True, help="Provide a node to connect to")
+    parser.add_argument("--user", action="store", required=True, help="SSH username to use for connection")
+    parser.add_argument("--key", action="store", required=True, help="Path to SSH private key file")
     args = parser.parse_args()
 
+    # Initialize SSH client session using the provided node IP, user, and key path
+    ssh = create_ssh_client(args.node, ssh_user=args.user, ssh_key_path=args.key)
