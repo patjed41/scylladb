bc1b0bb840bcf55d1f7c180353db23373a130dfc
Emil Maskovsky <emil.maskovsky@scylladb.com>
2025-05-20 11:50:25 +0200

    add the script shebang

--- scripts/hackaton/recover_script.py
+++ scripts/hackaton/recover_script.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 
 
@@ -5,5 +7,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Start a recover procedure")
     parser.add_argument("--node", action="store", required=True,help="Provide a node to connect to")
     args = parser.parse_args()
-
 
